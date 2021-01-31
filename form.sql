287e969af8a359622aa9439ee8b337738c9af8af
Mifaou <34101451+Mifaou@users.noreply.github.com>
2021-01-31 22:20:06 +0100

    Add files via upload

---
+++ form.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Jan 31, 2021 at 09:15 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.3.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `form`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `email`, `password`) VALUES
+(2, 'Oumaima Ouhaik', 'oumaima.ouhaik@hotmail.com', '123'),
+(4, 'Oumaima', 'z@z.com', '123');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ readme.txt
@@ -0,0 +1,15 @@
+<h2>Desc: </h2>
+This app allows you  to Add / Update / Delete user and all users is stored in a mysql database.
+Using :
+Mysql
+Nodejs
+Express
+Ejs
+1- Create a Mysql DB called form
+2-Import DB ( test.sql)
+2- Open config.js and insert your personal username and password for Mysql
+3- Open terminal in your current folder and install packages (Node Modules) ( i shared with u in case )
+4- Run your server using node app
+5- Open your browser and put http://localhost:3000( you can change port if you want )
+6- Finally your app is ready !! 
+If you need any help  contact me : oumaima.ouhaik@hotmail.com
